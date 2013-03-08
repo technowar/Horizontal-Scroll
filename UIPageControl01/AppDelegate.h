b184f9feefa757d560c3f134e8f873de4d7227a2
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2013-03-08 13:41:42 +0800

    Initial commit.

---
+++ UIPageControl01/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UIPageControl01
+//
+//  Created by OSX on 3/3/13.
+//  Copyright (c) 2013 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
